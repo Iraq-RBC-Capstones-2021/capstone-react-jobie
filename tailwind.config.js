03c94a954f54cda7c285c903d5305386caebf68c
ayasalam0 <ayasalam020@gmail.com>
2021-10-05 23:03:23 +0300

    tailwind extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#EC7063",
+        light: "#E7ECF2",
+        primary: "#72451",
+        secondary: "#154360",
+      },
+    },
   },
   variants: {
     extend: {},

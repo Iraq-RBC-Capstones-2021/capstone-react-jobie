7e548942d284ae0664b7080f1a401d797bb5304a
Wisam Naji <guitar90rocker@gmail.com>
2021-10-12 15:06:30 +0000

    tweaks

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import { wrapper } from "../store";
+import Layout from "../components/layout";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default wrapper.withRedux(App);

290c7a0b3ea1f8599005993f46203d020c7f090c
Rebaz Farid <rebaz415@gmail.com>
2021-10-13 16:01:27 +0300

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./footer";
+import Navbar from "./navbar";
 
 export default function Layout({ children }) {
   return (

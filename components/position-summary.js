147f08b92b0d8ca84b72d54a7c7192c9adb1bb59
bnar98 <bnar.paskandy@gmail.com>
2021-10-13 06:16:43 +0300

    create the position summary component Fixes #12

--- components/position-summary.js
+++ components/position-summary.js
@@ -4,6 +4,20 @@ import { FaUserAlt } from "react-icons/fa";
 import { FaLightbulb } from "react-icons/fa";
 import { FaLayerGroup } from "react-icons/fa";
 
+const summary = {
+  salaryMin: 45000,
+  salaryMax: 180000,
+  currency: {
+    name: "United States Dollars",
+    symbolShort: "$",
+    symbolLong: "USD",
+  },
+  gender: "any",
+  experience: "2+ years",
+  workLevel: "senior",
+  category: "Web Development",
+};
+
 export default function PositionSummary() {
   return (
     <div className=" bg-light h-auto w-80">
@@ -13,7 +27,13 @@ export default function PositionSummary() {
         </div>
         <div className="-ml-20">
           <h1 className="text-xl text-secondary font-bold">offered salary</h1>
-          <h1 className="text-base">$45K-$180K</h1>
+          <h1 className="text-base">
+            {summary.salaryMin +
+              summary.currency.symbolShort +
+              "-" +
+              summary.salaryMax +
+              summary.currency.symbolShort}
+          </h1>
         </div>
       </div>
       <div className="ml-5 grid grid-cols-2 w-54 mt-5 mb-6">
@@ -22,7 +42,7 @@ export default function PositionSummary() {
         </div>
         <div className="-ml-20">
           <h1 className="text-xl text-secondary font-bold">Gender</h1>
-          <h1 className="text-base">Any</h1>
+          <h1 className="text-base">{summary.gender}</h1>
         </div>
       </div>
       <div className="ml-5 grid grid-cols-2 w-54 mt-5 mb-6">
@@ -31,7 +51,7 @@ export default function PositionSummary() {
         </div>
         <div className="-ml-20">
           <h1 className="text-xl text-secondary font-bold">Experience</h1>
-          <h1 className="text-base">+2 Years</h1>
+          <h1 className="text-base">{summary.experience}</h1>
         </div>
       </div>
       <div className="ml-5 grid grid-cols-2 w-54 mt-5 mb-6">
@@ -40,7 +60,7 @@ export default function PositionSummary() {
         </div>
         <div className="-ml-20">
           <h1 className="text-xl text-secondary font-bold">Work Level </h1>
-          <h1 className="text-base"> senior</h1>
+          <h1 className="text-base"> {summary.workLevel}</h1>
         </div>
       </div>
       <div className="ml-5 grid grid-cols-2 w-54 mt-5 mb-6">
@@ -49,7 +69,7 @@ export default function PositionSummary() {
         </div>
         <div className="-ml-20">
           <h1 className="text-xl text-secondary font-bold">Category</h1>
-          <h1 className="text-base">Web development</h1>
+          <h1 className="text-base">{summary.category}</h1>
         </div>
       </div>
     </div>

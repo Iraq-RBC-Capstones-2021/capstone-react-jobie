6ae437a175009ba8f7b1b2d4feb03f7b3e206be8
Ge6ben <dl22gesh40@gmail.com>
2021-10-15 21:49:39 +0300

    create user profile header Fixes #10

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,7 @@ export default function Home() {
           </a>
         </div>
       </main>
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"

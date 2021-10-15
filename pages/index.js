5c09aae4924e93809031d0e7d5e268fee1f86db7
Ge6ben <dl22gesh40@gmail.com>
2021-10-15 21:31:28 +0300

    create user profile header Fixes #10

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import HeaderProfile from "../components/HeaderProfile";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <HeaderProfile />
       <Head>
         <title>Create Next App</title>
 

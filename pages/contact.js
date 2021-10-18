ab618d6171431277c5bcae534d39a2b663e5e48b
rebaz.36 <rebaz415@gmail.com>
2021-10-18 16:24:55 +0300

    Fixes to contact us page Fixes #64

--- pages/contact.js
+++ pages/contact.js
@@ -6,14 +6,14 @@ import { IoTimeOutline } from "react-icons/io5";
 import pic from "../assets/img_contact.png";
 export default function Home() {
   return (
-    <div className="p-32 bg-body text-dark">
+    <div className=" bg-body text-dark px-4 lg:px-48 w-full">
       <div className="pb-10">
-        <img alt="section image" src={pic.src} className="ml-auto h-full" />
+        <img alt="section image" src={pic.src} className="h-full" />
       </div>
 
-      <div className="grid grid-cols-2 gap-32 font-medium ">
+      <div className="grid grid-cols-2 font-medium p-6 ">
         <div className="col-1">
-          <h1 className="pb-8">Get in Touch</h1>
+          <h1 className="pb-8 lg:text-4xl">Get in Touch</h1>
 
           <form className="form mr-8 ">
             <div className="flex space-x-5 mt-3">
@@ -48,7 +48,7 @@ export default function Home() {
 
             <button
               type="submit"
-              className=" text-xl rounded-full  mt-6 p-2 pr-10 pl-10  text-white font-semibold  bg-accent"
+              className=" text-xl lg:text-2xl  rounded-full  mt-6 p-2 pr-10 pl-10  text-white font-semibold  bg-accent"
             >
               {" "}
               Send{" "}
@@ -56,18 +56,18 @@ export default function Home() {
           </form>
         </div>
 
-        <div className="col-2 ml-20">
-          <h1 className="pb-8">Contact Us</h1>
+        <div className="col-2 ml-auto w-3/4 lg:w-4/5 xl:w-2/3 ">
+          <h1 className="pb-8 lg:text-4xl">Contact Us</h1>
 
-          <div className="grid grid-cols-3 mt-3 p-12 bg-lightgrey ">
-            <div className="col-1 grid grid-rows-4 gap-6">
+          <div className="grid grid-cols-3  mt-3 p-6 lg:p-6 xl:p-12 bg-lightgrey ">
+            <div className="col-1 grid grid-rows-4 gap-6 justify-around">
               <IoLocationOutline className="row-1 w-10 h-10 " />
               <IoCallOutline className=" row-2 w-10 h-10 " />
               <IoMailOutline className="row-3 w-10 h-10 " />
               <IoTimeOutline className="row-4 w-10 h-10 " />
             </div>
 
-            <div className="col-2 col-span-2 grid grid-rows-4 gap-6 ">
+            <div className="col-2 col-span-2 grid grid-rows-4 gap-6 justify-around ">
               <h3 className="row-1 "> As Sulaymaniyah, Iraq </h3>
               <h3 className="row-2"> +964 750 113 0495 </h3>
               <h3 className="row-3"> Jobie@contact.com</h3>

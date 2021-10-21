8255a72038378eab4935d2a134ea9d2b017930f9
Zahraa <sahrayarub@yahoo.de>
2021-10-21 19:52:43 +0300

    set up firebase

---
+++ config/dbConfig.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAD6FrZT5QqBTIvS8-HBzNC1T5wNGbrAE",
+  authDomain: "capstone-react-jobie.firebaseapp.com",
+  projectId: "capstone-react-jobie",
+  storageBucket: "capstone-react-jobie.appspot.com",
+  messagingSenderId: "519897798533",
+  appId: "1:519897798533:web:e658f997be067cadc3d6ec",
+  measurementId: "G-468DW16S80",
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;

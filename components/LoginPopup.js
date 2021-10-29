3afb82a5ec7143319123fe07559821ffce56c278
Zahraa <sahrayarub@yahoo.de>
2021-10-29 17:27:32 +0300

    connect popup to navbar

--- components/LoginPopup.js
+++ components/LoginPopup.js
@@ -1,22 +1,47 @@
 import logo from "../assets/img_logo.png";
 import { FcGoogle } from "react-icons/fc";
-export default function LoginPopup() {
-  return (
-    <div className="border border-4 rounded-md shadow-md p-6  m-12 w-1/4">
-      <div className="flex flex-col items-center ">
-        <div className="text-center w-32 py-8">
-          <img
-            src={logo.src}
-            alt="no Image"
-            className="object-center w-full h-full "
-          />
-        </div>
-        <div className="my-15 bg-googleBtnBlue rounded-sm flex items-center justify-between w-52 py-0.25 px-0.25 mb-9 ">
-          <span className="bg-white p-2 m-1 border border-4 rounded-sm ">
-            <FcGoogle size={27} />
-          </span>
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { registerWithGoogle } from "../store/auth/authSlice";
 
-          <span className="text-white px-2 ">Sign in with Google</span>
+export default function LoginPopup({ handleLoginModal, showLoginModal }) {
+  const dispatch = useDispatch();
+  const handleLoginClick = () => {
+    dispatch(registerWithGoogle());
+    handleLoginModal();
+  };
+
+  return (
+    <div
+      className={`bg-black bg-opacity-80 inset-0 fixed flex justify-center items-center z-50 h-full w-full ${
+        !showLoginModal && "hidden"
+      }`}
+    >
+      <div className="rounded-md shadow-md w-1/4 bg-white">
+        <div className="flex items-end  justify-end">
+          <button className="p-4 text-xl" onClick={handleLoginModal}>
+            <FaTimes />
+          </button>
+        </div>
+        <div className="flex flex-col items-center justify-center px-6 space-y-8 pb-12">
+          <div className="text-center w-32">
+            <img
+              src={logo.src}
+              alt="Jobie Logo"
+              className="object-center w-full h-full "
+            />
+          </div>
+          <button
+            className="my-15 bg-googleBtnBlue rounded-sm flex items-center justify-between py-0.25 px-0.25"
+            onClick={handleLoginClick}
+          >
+            <span className="bg-white p-1 m-1 rounded-sm ">
+              <FcGoogle size={45} />
+            </span>
+
+            <span className="text-white px-2 ">Sign in with Google</span>
+          </button>
         </div>
       </div>
     </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import NavBarSearch from "./NavBarSearch";
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 
+import { FaAngleDown } from "react-icons/fa";
+import LoginPopup from "./LoginPopup";
+import LoginAccountTpe from "./LoginAccountType";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutGoogle } from "../store/auth/authSlice";
+
 function Navbar() {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [showLoginModal, setShowLoginModal] = useState(false);
   const [menuActive, setMenuActive] = useState(false);
+  const [accountMenuActive, setAccountMenuActive] = useState(false);
+
+  const accountMenuRef = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (!accountMenuRef.current.contains(event.target))
+        setAccountMenuActive(false);
+    };
+    if (accountMenuActive) {
+      document.addEventListener("mousedown", handler);
+    }
+
+    // clear up event listener
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [accountMenuActive]);
+
   const onClick = () => {
     setMenuActive(!menuActive);
   };
 
+  const handleAccountMenu = () => {
+    setAccountMenuActive(!accountMenuActive);
+  };
+
+  const handleLoginModal = () => {
+    setShowLoginModal(!showLoginModal);
+  };
+
+  const handleLogoutClick = (e) => {
+    e.preventDefault();
+    setAccountMenuActive(false);
+    dispatch(logoutGoogle());
+  };
+
   return (
     <>
+      <LoginPopup
+        handleLoginModal={handleLoginModal}
+        showLoginModal={showLoginModal}
+      />
+      {!auth.profileCompleted && auth.currentUser !== null ? (
+        <LoginAccountTpe />
+      ) : (
+        ""
+      )}
       <header className="relative border-b border-solid  border-gray-300 ">
         <div className="max-w-7xl mx-auto flex items-center justify-between p-2.5">
           <div>
@@ -58,17 +108,69 @@ function Navbar() {
               </li>
 
               <li className="list-none">
-                <Link href="/login">
-                  <a className="flex w-full text-base px-2.5">
-                    {" "}
+                {auth.currentUser === null ? (
+                  <button
+                    type="button"
+                    className=" text-white rounded-full px-7 py-1 bg-accent"
+                    onClick={handleLoginModal}
+                  >
+                    Login
+                  </button>
+                ) : (
+                  <div className="relative inline-block" ref={accountMenuRef}>
                     <button
                       type="button"
-                      className=" text-white rounded-full px-7 py-1 bg-accent"
+                      className=" text-white rounded-full px-7 py-1 bg-accent inline-flex justify-center items-center"
+                      onClick={handleAccountMenu}
                     >
-                      Login
+                      Account
+                      <FaAngleDown />
                     </button>
-                  </a>
-                </Link>
+                    <div
+                      className={`origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                        !accountMenuActive && "hidden"
+                      }`}
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="menu-button"
+                      tabIndex="-1"
+                    >
+                      <Link href="#">
+                        <a
+                          className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-200"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-0"
+                        >
+                          View Profile
+                        </a>
+                      </Link>
+                      <Link href="#">
+                        <a
+                          className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-200"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-1"
+                        >
+                          Edit Profile
+                        </a>
+                      </Link>
+
+                      <div className="py-1 " role="none">
+                        <button
+                          type="submit"
+                          className="text-gray-700 block w-full text-left px-4 py-2 text-sm hover:bg-gray-200 border-t border-gray-200"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-3"
+                          onClick={handleLogoutClick}
+                        >
+                          Logout
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </li>
             </ul>
           </nav>

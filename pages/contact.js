2820afaf9cd5bbe0b5598daa1a53731e7e195955
rebaz36 <rebaz415@gmail.com>
2021-10-30 19:10:14 +0300

    -_- does not submit

--- pages/contact.js
+++ pages/contact.js
@@ -1,36 +1,54 @@
+import react, { useRef } from 'react';
 import { IoLocationOutline } from "react-icons/io5";
 import { IoCallOutline } from "react-icons/io5";
 import { IoMailOutline } from "react-icons/io5";
 import { IoTimeOutline } from "react-icons/io5";
+import emailjs from 'emailjs-com';
 
 import pic from "../assets/img_contact.png";
 export default function Home() {
+
+  const form = useRef();
+
+  function sendFeedback(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_gp268sw', form.current, 'user_sRP5iBhZmFEbxe9NtZU2b')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
   return (
     <div className=" bg-body text-dark px-4 lg:px-48 w-full">
       <div className="pb-10">
-        <img alt="section image" src={pic.src} className="h-full" />
+        <img alt="section image" src={pic.src} className="w-full" />
       </div>
 
       <div className="grid grid-cols-2 font-medium p-6 ">
         <div className="col-1">
           <h1 className="pb-8 lg:text-4xl">Get in Touch</h1>
 
-          <form className="form mr-8 ">
-            <div className="flex space-x-5 mt-3">
+          <form className="form mr-8 "ref={form} onSubmit={sendFeedback}>
+            <label className="flex space-x-2 my-3">
+              First Name
               <input
                 name = "first-name"
                 type="text"
                 placeholder="First Name"
-                className="border p-2  w-1/2 border-dark"
+                className="border p-2 mr-4  w-1/2 border-dark"
               />
+              Last Name
               <input
                 name ="last-name"
                 type="text"
                 placeholder="Last Name"
                 className="border p-2 w-1/2 border-dark"
               />
-            </div>
+            </label>
 
+            <label>Email</label>
             <input
               type="email"
               name="email"
@@ -38,23 +56,17 @@ export default function Home() {
               placeholder="Your Email"
               className="border p-2 w-full mt-3 border-dark"
             />
-
+            <label> Message </label>
             <textarea
               name="message"
-              id=""
-              cols="10"
-              rows="3"
-              placeholder="How can we help?"
               className="border p-2 mt-3 w-full border-dark"
             />
 
-            <button
-              type="submit"
-              className=" text-xl lg:text-2xl  rounded-full  mt-6 p-2 pr-10 pl-10  text-white font-semibold  bg-accent"
-            >
-              {" "}
-              Send{" "}
-            </button>
+            <input 
+              type="submit" 
+              value="Send" 
+              className=" text-xl lg:text-2xl  rounded-full  mt-6 p-2 pr-10 pl-10  text-white font-semibold  bg-accent" 
+            />
           </form>
         </div>
 

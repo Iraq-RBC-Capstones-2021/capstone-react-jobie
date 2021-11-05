5f419299876a9a5281d9b831f51a72a33e8ea24f
rebaz36 <rebaz415@gmail.com>
2021-11-05 21:09:56 +0300

    Finished

--- locales/en.js
+++ locales/en.js
@@ -1,4 +1,5 @@
 export default {
+
   about:{
     HTitle:"Find Your Job",
     HSubTitle:"Live your Dream",
@@ -50,6 +51,7 @@ export default {
     OurCollaborators2: "Collaborators",
   },
 
+
   contact:{
     GetInTouch: "Get in touch",
     FirstName: "First Name",
@@ -64,6 +66,7 @@ export default {
     EmailUs: "Jobie@contact.com",
     OpenHours: "9:00 - 17:00",
   },
+
 
   home:{
     HTitle1:"10,254 Jobs",
@@ -83,7 +86,6 @@ export default {
     RemotesJobs: "Remotes Jobs",
     RemotesJobsDescription: "if you are set on your career and want to work remotes, then a remotes job is just the one for you, clicking here will show you all the current remotes jobs in our platform",
     ViewAllRemotesJobs: "View all remotes jobs",
-
 
     Categories: "Explore by categories",
 
@@ -109,7 +111,6 @@ export default {
     DataAnalyzerNumbers: "342 Job Vacancy",
 
     ViewAllCategories: "View All Categories",
-
 
     LatestJobs: "Latest Jobs",
     Position: "Position",
@@ -137,6 +138,7 @@ export default {
     UsingOurServices: "More than 20,000 companies are using our service to hire their employees."
   },
 
+
   roadmap:{
     Htitle: "Developer Roadmaps",
     HSubTitle: "Follow these roadmaps and become a hero in any path you choose!",
@@ -156,6 +158,7 @@ export default {
     Security: "Security",
     Cloud: "Cloud",
   },
+
 
   jobs:{
     JobsFider: "Jobs Finder",
@@ -177,6 +180,7 @@ export default {
     SortBy: "Sort By: Newest",
   },
 
+
   navbar:{
     Home: "Home",
     About: "About",
@@ -186,6 +190,8 @@ export default {
     Search: "Search",
     Login: "Login",
   },
+
+
   footer:{
     JobieDesc: "Jobie is a platform that can help you set up your career we give services from finding out the right path to advance your career through a roadmap or a right position from the top companies",
     
@@ -211,104 +217,78 @@ export default {
     RateAnEmployee: "Rate An Employer",
   },
 
-  title: "jobie",
-  headerJobs: "10,254 Jobs",
-  AreListedHere: "Are Listed Here!",
-  Findyourdreamjobnow: "Find your dream job now",
-  FullTimeJobs: "Full-Time Jobs",
-  FirstJobCard:
-    "if you are set on your career and want to work full time, then a full time job is just the one for you, clicking here will show you all the current full time jobs in our platform",
-  PartTime: "Part Time Jobs",
-  PartTimeJobs:
-    "if you're a student or can't spend to much time on work, then a part time job is what you're looking for. click here and we'll show you all the part-time job offers",
-  remote: "Remote Jobs",
-  remoteParagraph:
-    "if you prefer working from home, with no constraints to a specific place then a remote job is a perfect fit and we've got plenty of that, click and let us show you all the available positions",
-  explor: " Explore by categories",
-  Design: "Design",
-  designNumbers: "67 Job Vacancy",
-  fronEnd: "Frontend",
-  frontNumber: "180 Job Vacancy",
-  backend: "Backend",
-  backendNumber: "78 Job Vacancy",
-  ZahraRole: "Senior Developer | Designer",
-  RebazRole: "Project manager | Developer",
-  WisamRole: "Supervisor | Lead Engineer",
-  AyaRole: "Developer | Translator",
-  BnarRole: "Developer | Designer",
-  LaraRole: "Developer | Designer",
-  webDevNumber: "233 Job Vacancy",
-  network: "Network",
-  networkNumber: "53 Job Vacancy",
-  PM: "Project Manager",
-  PMnumber: "137 Job Vacancy",
-  data: "Data",
-  dataNumber: "164 Job Vacancy",
-  viewCatg: "View all Categories ",
-  latestJobs: "Latest Jobs",
-  NoJobsFound: "No jobs found",
-  costumers: "Our Customers",
-  helped: "We’ve helped 2,568 users to find their dream job.",
-  aya: "Aya S. mohammad",
-  ayaPosition: "CEO Co-Founder, Jobie",
-  feedback:
-    "Jobie is always true to its internal mission statement: Putting the job seeker first. The mission does not waiver, and all decisions are made to create a better world with job seekers who feel that they can make good decisions for their future.",
-  rebaz: "Rebaz Farid",
-  zahraa: "Zahraa YH",
-  wisam: "Wisam Naji",
-  companiesUsing:
-    "More than 20,000 companies are using our service to hire their employees.",
-  fullTimeTable: "Full Time",
-  partTimeTable: "Part Time",
-  remoteTable: "remote",
-  PositionTH: "Position",
-  SalaryTH: "Salary",
-  DateTH: "Date",
-  ApplyTH: "Apply",
-  Position: "UI/UX Designer",
-  Salary: "5000$",
-  Date: "3 hours ago",
-  Home: "Home",
-  About: "About",
-  Roadmaps: "Roadmaps",
-  Jobs: "Jobs",
-  Contact: "Contact",
-  Login: "Login",
-  Account: "Account",
-  ViewProfile: " View Profile",
-  EditProfile: "EditProfile",
-  Logout: "Logout",
-  Viewall: "Viewall",
-  aboutHeader: "Find Your Job",
-  aboutSub: "Live Your Dream ",
-  about: "About",
-  us: "us",
-  mission:
-    " Our Mission is to build a bridge between professionals and  employers those who need a job and the ones who needs someone to do the job for them this platform makes connections easier and faster, in a community where the tech sector is growing rapidly the demand for tech professionals is growing as well We strives to put job seekers first giving them free access to search for jobs post resumes and research companies. Every day we connect millions of people to new opportunities. but of course this also allows companies find the right candidate faster putting themselves out there and allowing job seekers to see their open positions.",
-  howWork: " How it works",
-  title1: "Create your resume",
-  title2:
-    "creating your resume helps you apply faster and showcase your experience and skills to employers",
-  title3: "Apply for a Job",
-  title4:
-    "Through our website you can find different kind of jobs from different tech categories and applying is made easy once you set up your resume",
-  title5: "Get hired now",
-  title6:
-    "Once you apply for a job the employer will be able to see your resume and the position you applied for, which makes hiring easy",
-  choose: "Why Choose ",
-  us: "Us?",
-  posted: "Posted",
-  vaccine: "15k Vacancies",
-  saved: "Saved",
-  vaccine2: "5k Vacancies",
-  submitted: "Submitted",
-  cv: "12k CV",
-  hired: "Hired",
-  noHired: "8k Employees",
-  meet: "Meet Our",
-  team: "team",
-  lara: "Lara Raaof",
-  banar: " Banar Kamal",
-  our: "Our",
-  Collaborators: "Collaborators",
-};
+
+  JobView: {
+    ViewAllJobs: "View All Jobs",
+    ApplyNow: "Apply Now",
+    Details: "Details",
+    JobDescription: "Job Description",
+    Responsibilities: "Responsibilities Include",
+    Background: "Background & Experience",
+    SimilarJobs: "Similar Jobs",
+
+    OfferedSalary: "Offered Salary",
+    Gender: "Gender",
+    Experience: "Experience",
+    WorkLevel: "Work Level",
+    Category: "Category",
+  },
+
+
+  ProfileView:{
+    Hire: "Hire",
+    DownloadCV: "Download CV",
+    Bio: "No Biography added yet",
+    Contact: "Contact",
+    Skills: "Skills",
+    WorkExperience: "Work Experience",
+    Education: "Education",
+  },
+
+
+  ProfileEdit:{
+    UploadPhoto: "Upload Photo",
+    Delete:"Delete",
+    ImageDesc:"Image should be at least 400 x 400px as a png or jpeg file",
+    
+    ResumeFile:"Resume File",
+    Choose:"Choose",
+    NoFileSelected:"No File Selected",
+
+    BasicInfo:"Basic Info",
+    FirstName:"First Name",
+    LastName:"Last Name",
+    Title:"Title",
+    Biography: "Biography",
+
+    Contact: "Contact",
+    Location: "Location",
+    Phone: "Phone",
+    Email: "Email",
+
+    Social: "Social",
+
+    Skills: "Skills",
+    SkillsDesc: "Add skills as comma seperated values.",
+
+    AddItem: "Add Item",
+    Remove: "Remove",
+
+    WorkExperience: "Work Experience",
+    ExperienceItem: "Experience Item",
+    Company: "Company",
+    Location: "Location",
+    TypeOfEmployment: "Type of Employment",
+    Position: "Position",
+    Date: "Date",
+
+    Education: "Education",
+    EducationItem: "Education Item",
+    UniversitySchool: "University / School",
+    Major: "Major",
+    Date: "Date",
+
+    Save: "Save",
+    Cancel: "Cancel",
+  },
+}
